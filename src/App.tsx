33659ebe145ba855f41f62dc725b3f83c16f5ecd
Alessandro Fugazza <137590109+alessandrofugazza@users.noreply.github.com>
2024-03-09 20:19:44 +0100

    create home component

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./assets/styles/App.scss";
+import { Home } from "./components/pages/Home";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>Like a dream &hellip;</p>
-        {/* ? rel="noopener noreferrer" */}
-        {/* <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+      {/* <header className="App-header"> */}
+      {/* ? rel="noopener noreferrer" */}
+      {/* <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a> */}
-      </header>
+      {/* </header> */}
+      <main>
+        <Home />
+      </main>
     </div>
   );
 }
---
+++ src/components/pages/Home.tsx
@@ -0,0 +1,7 @@
+export const Home = () => {
+  return (
+    <section className="home">
+      <p className="fade-in-and-move">Like a dream &hellip;</p>
+    </section>
+  );
+};
